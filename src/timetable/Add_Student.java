cc289f26eec75b12a4fbe08bb87f53d127641747
Btec-19825 <68671484+Btec-19825@users.noreply.github.com>
2021-06-02 20:01:46 +0530

    fourth commit

--- src/timetable/Add_Student.java
+++ src/timetable/Add_Student.java
@@ -341,9 +341,9 @@ public class Add_Student extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         
-        //Manage_Student C = new Manage_Student();
-        //this.hide();
-        //C.setVisible(true);
+        Manage_Student C = new Manage_Student();
+        this.hide();
+        C.setVisible(true);
         
     }//GEN-LAST:event_jButton1ActionPerformed
 
