b9516098a6e9e5b3dcdb68ffe9970c7887d24cc1
Uvini <it19005072@my.sliit.lk>
2021-06-02 09:26:46 +0530

    fifth commit

--- src/timetable/Manage_Sessions_Rooms.java
+++ src/timetable/Manage_Sessions_Rooms.java
@@ -293,9 +293,9 @@ public class Manage_Sessions_Rooms extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        //Timetable C = new Timetable();
-       //this.hide();
-       //C.setVisible(true);
+        Timetable C = new Timetable();
+       this.hide();
+       C.setVisible(true);
         
     }//GEN-LAST:event_jButton1ActionPerformed
 
